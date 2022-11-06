3b781fbd4f9289a02b930f7c8766a4bfd39b7a86
Debabrata Ghosh <rijugh@gmail.com>
2022-11-06 14:55:04 +0100

    updated with assignment 01 solutions

---
+++ build/extern/glm/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/drtghosh/Documents/All_of_RWTH/Semester1/Courses/Graphics/Assignments/basic-techniques/extern/glm
+# Build directory: C:/Users/drtghosh/Documents/All_of_RWTH/Semester1/Courses/Graphics/Assignments/basic-techniques/build/extern/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
---
+++ build/extern/glm/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/drtghosh/Documents/All_of_RWTH/Semester1/Courses/Graphics/Assignments/basic-techniques/extern/glm/glm
+# Build directory: C:/Users/drtghosh/Documents/All_of_RWTH/Semester1/Courses/Graphics/Assignments/basic-techniques/build/extern/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
